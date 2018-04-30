54f4a8befae2b5969ebfda0baeb6259f640f752e
Dan Kingswell <dankingswell22@gmail.com>
2018-04-30 21:46:03 +0100

    made inital routes

--- app.py
+++ app.py
@@ -12,9 +12,20 @@ app = Flask(__name__)
 
 
 
-@app.route("/")
+@app.route("/login",methods=["GET","POST"])
+def login():
+    pass
+
+@app.route("/",methods=["GET"])
 def index():
     pass
+
+@app.route("/register", methods=["GET","POST"])
+def register():
+    pass
+
+
+
 
 if __name__ == "__main__":
     app.run(debug=True)
