e7fec74ddeb7f795d76a07356083d0ebb5571ba3
Dan Kingswell <dankingswell22@gmail.com>
2018-05-03 18:10:10 +0100

    Switching config to use Raw SQL instead of ORM

--- app.py
+++ app.py
@@ -1,18 +1,15 @@
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
 from werkzeug import generate_password_hash,  check_password_hash
 from functools import wraps
 import uuid
 import datetime
 import security
-
+import Models 
 
 app = Flask(__name__)
-
 app.config.from_pyfile("config.py")
 
 db = SQLAlchemy(app)
-
 from routes import *
 
 
--- routes.py
+++ routes.py
@@ -3,9 +3,13 @@ from app import app
 import security
 
 
+
 @app.route("/login",methods=["GET","POST"])
 def login():
-    pass
+    if request.method == "GET":
+        return render_template("loginform.html")
+    if request.method == "POST":
+        return url_for("/")
 
 @app.route("/",methods=["GET"])
 def index():
@@ -13,7 +17,8 @@ def index():
 
 @app.route("/register", methods=["GET","POST"])
 def register():
-    pass
+    if request.method == "GET":
+        return render_template("registerform.html")
 
 @app.route("/createworkout",methods=["GET","POST"])
 def createworkout():
